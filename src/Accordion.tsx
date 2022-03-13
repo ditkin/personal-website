4fe16cee8065e238d18e1b1ce552c53fb3c25403
David Itkin <ditkin@hubspot.com>
2022-03-13 11:07:42 -0400

    Add build files

--- src/Accordion.tsx
+++ src/Accordion.tsx
@@ -7,17 +7,21 @@ export default function Accordion({
 }) {
   return (
     <>
-      <button style={{
-        background-color: '#eee',
-        color: '#444',
-        cursor:'pointer',
-        padding:'18px',
-        width: '100%',
-        text-align:'left',
-        border:'none',
-        outline:'none',
-        transition: '0.4s',
-      }}>Section 1</button>
+      <button
+        style={{
+          backgroundColor: '#eee',
+          color: '#444',
+          cursor: 'pointer',
+          padding: '18px',
+          width: '100%',
+          textAlign: 'left',
+          border: 'none',
+          outline: 'none',
+          transition: '0.4s',
+        }}
+      >
+        Section 1
+      </button>
       <div class="panel">{children}</div>
     </>
   );
